1ce6dec779a3b059f2f5083ef6a1cba9c87520a6
hirosuku0925 <mu6san1ko@gmail.com>
2026-06-01 10:36:02 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({ width: 1000, height: 700 });
+  win.loadURL('https://nijin.app');
+}
+
+app.whenReady().then(createWindow);
